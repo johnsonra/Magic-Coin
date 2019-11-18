6c70132972f4b1d28c2bbd2add3f798e5cb528c9
Randy Johnson <johnsonra@mail.nih.gov>
2019-11-18 15:29:22 -0500

    First pass at check_magic_seed.R

---
+++ check_magic_seed.R
@@ -0,0 +1,9 @@
+# check_magic_seed.R
+# This code checks the seed found in find_magic_seed.R
+
+# ** don't edit this file ** #
+
+source('find_magic_seed.R')
+
+if(!is_magic_seed(s))
+    stop('Sorry, no magic - try again.')
